bf731cdf60780823deb1a658dd2e3c6d216bbd13
Vladislad T <v.trundenko@everad.com>
2018-12-26 16:22:52 +0200

    vlad

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = [
-    './source/js/app.js',
-  './source/js/everad.js',
-  './source/js/postDate.js'
+  './source/js/app.js',
+  './source/js/slick.min.js',
+  './source/js/jquery-3.3.1.min.js',
+  './source/js/slick.min.js'
 ];
--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -2,6 +2,5 @@
 
 module.exports = [
   './node_modules/normalize.css/normalize.css',
-  './bower_components/wow/css/libs/animate.css',
-  './bower_components/owl.carousel/dist/assets/owl.carousel.min.css'
+  './bower_components/wow/css/libs/animate.css'
 ];
