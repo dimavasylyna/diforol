3c102d93ddbb4aa5b2d6ca7f8b11405b4b3f523f
Vladislad T <v.trundenko@everad.com>
2018-12-26 16:32:09 +0200

    vlad

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -4,5 +4,6 @@ module.exports = [
   './source/js/app.js',
   './source/js/slick.min.js',
   './source/js/jquery-3.3.1.min.js',
-  './source/js/slick.min.js'
+  './source/js/slick.min.js',
+   './source/js/everad.js'
 ];
